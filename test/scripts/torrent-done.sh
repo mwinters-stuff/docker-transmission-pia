adc4adfa550157b8c85379c9b50cd0dc1d396e8d
Mathew Winters <mathew@winters.org.nz>
2022-06-28 19:50:48 +1200

    update image with entrypoint for pia.

--- test/scripts/torrent-done.sh
+++ test/scripts/torrent-done.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /scripts
+cd /scripts || exit 1
 # Change this if you wish to log the verification
 LOG_FILE=/downloads/torrent-end.log
 export HOME=/scripts
@@ -8,25 +8,26 @@ export HOME=/scripts
 # TR_TORRENT_NAME=$2
 # TR_TORRENT_DIR=$3
 touch $LOG_FILE
-echo "RUN: $@" >> $LOG_FILE
-echo "TR_TORRENT_DIR=${TR_TORRENT_DIR}" >> $LOG_FILE
-echo "TR_TORRENT_ID=${TR_TORRENT_ID}"  >> $LOG_FILE
-echo "TR_TORRENT_NAME=${TR_TORRENT_NAME}" >> $LOG_FILE
-
+{ 
+  echo "RUN: ${*}" 
+  echo "TR_TORRENT_DIR=${TR_TORRENT_DIR}" 
+  echo "TR_TORRENT_ID=${TR_TORRENT_ID}"
+  echo "TR_TORRENT_NAME=${TR_TORRENT_NAME}"
+} >> $LOG_FILE
 
 function startTorrent {
   echo "Re-Starting ${TR_TORRENT_NAME}" >> $LOG_FILE
-  transmission-remote -t ${TR_TORRENT_ID} --start >> $LOG_FILE
+  transmission-remote -t "${TR_TORRENT_ID}" --start >> $LOG_FILE
 }
 
 function stopTorrent {
   echo "Stopping ${TR_TORRENT_NAME}" >> $LOG_FILE
-  transmission-remote -t ${TR_TORRENT_ID} --stop >> $LOG_FILE
+  transmission-remote -t "${TR_TORRENT_ID}" --stop >> $LOG_FILE
 }
 
 function deleteTorrent {
   echo "Deleting ${TR_TORRENT_NAME}" >> $LOG_FILE
-  transmission-remote -t ${TR_TORRENT_ID} --remove-and-delete >> $LOG_FILE
+  transmission-remote -t "${TR_TORRENT_ID}" --remove-and-delete >> $LOG_FILE
 }
 
 
@@ -48,15 +49,15 @@ cat $TEMPLOG >>  $LOG_FILE
 echo "RESULT $tvrrv" >> $LOG_FILE
 if [ $tvrrv -eq 0 ]
 then
-  grep -i "processed successfully" $TEMPLOG >> $LOG_FILE
-  if [ $? -eq 0 ]
+  if grep -i "processed successfully" $TEMPLOG >> $LOG_FILE
   then
     echo "Rename Success  ${TR_TORRENT_DIR}" >> $LOG_FILE
     deleteTorrent
-    /usr/bin/curl "http://192.168.5.35:32400/library/sections/4/refresh?X-Plex-Token=dqG18RQoR2FMWqUX2pTT" >> $LOG_FILE 2>&1 
+    /usr/bin/curl "https://xelp.winters.nz:32400/library/sections/1/refresh?X-Plex-Token=dqG18RQoR2FMWqUX2pTT" >> $LOG_FILE 2>&1 
+    /usr/bin/curl "https://xelp.winters.nz:32400/library/sections/4/refresh?X-Plex-Token=dqG18RQoR2FMWqUX2pTT" >> $LOG_FILE 2>&1 
   else
     echo "Rename Failed" >> $LOG_FILE
-    echo "RUN: $@" >> $LOG_FILE
+    echo "RUN: ${*}" >> $LOG_FILE
   fi
 else
   echo "Rename Failed ${tvrrv} ${TR_TORRENT_DIR}/${TR_TORRENT_NAME}" >> $LOG_FILE
