aac92c947cc444d1f95e62007fc4f9e4f8ee73fd
Mathew Winters <mathew@winters.org.nz>
2022-07-18 12:45:25 +1200

    fix and make work!

--- rename-server/bin/server.dart
+++ rename-server/bin/server.dart
@@ -5,14 +5,20 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:path/path.dart' as p;
-import 'package:http/http.dart' as http;
+import 'package:http/io_client.dart';
 import 'package:xml/xml.dart';
 
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
   ..get('/echo/<message>', _echoHandler)
-  ..get('/torrent-done/<torrent_id>/<torrent_name>', _torrentDoneHandler);
+  ..get('/error', _errorHandler)
+  ..get('/torrent-done/<torrent_name>', _torrentDoneHandler)
+  ..get('/refresh-plex', _refreshPlex);
+
+Response _errorHandler(_) {
+  return Response.notFound('no');
+}
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
@@ -24,13 +30,15 @@ Response _echoHandler(Request request) {
 }
 
 Response _torrentDoneHandler(Request request) {
-  final torrentID = request.params['torrent_id'];
-  final torrentName = request.params['torrent_name'];
-  final torrentDir = Platform.environment['TORRENT_DIR'] ?? '/downloads/complete';
+  final torrentName = Uri.decodeComponent(request.params['torrent_name'].toString());
+  final torrentDir = Platform.environment['TORRENT_DIR'] ?? '/downloads';
   final scriptsDir = Platform.environment['SCRIPTS_DIR'] ?? '/scripts';
-  print('Running mnamer on torrent $torrentName $torrentName');
+  print('Running mnamer on $torrentName');
 
-  final result = Process.runSync('mnamer', ['--config-path=${p.join(scriptsDir, "mnamer.json")}', '-b', '--verbose', '--no-style', p.join(torrentDir, torrentName)]);
+  final result = Process.runSync('mnamer', ['--config-path=${p.join(scriptsDir, "mnamer.json")}', '-b', '--verbose', '--no-style', p.join(torrentDir, 'complete', torrentName)]);
+
+  print(result.stdout.toString());
+  print(result.stderr.toString());
 
   File(p.join(torrentDir, 'mnamer.log')).writeAsStringSync(result.stdout.toString(), mode: FileMode.append);
 
@@ -38,41 +46,47 @@ Response _torrentDoneHandler(Request request) {
     return Response.notFound(result.stdout);
   }
   _updatePlex();
-  return Response.ok('$torrentID\n$torrentName\n$torrentDir\n');
+  return Response.ok('$torrentName\n');
 }
 
-void _updatePlex() {
+Response _refreshPlex(_) {
+  _updatePlex();
+  return Response.ok('ok\n');
+}
+
+void _updatePlex() async {
   final plexServerAddress = Platform.environment['PLEX_SERVER_ADDRESS'];
   final plexToken = Platform.environment['PLEX_TOKEN'];
 
-  if (plexServerAddress == null || plexToken == null) {
+  if (plexServerAddress == null || plexToken == null || plexServerAddress.isEmpty || plexToken.isEmpty) {
     print("Plex env not set not updating plex");
     return;
   }
 
-  var client = http.Client();
+  final httpClient = HttpClient()..badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+  // var client = http.Client();
+  final ioClient = IOClient(httpClient);
   try {
     var url = Uri.https(plexServerAddress, '/library/sections/', {'X-Plex-Token': plexToken});
-    client.get(url).then((response) {
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
-      final document = XmlDocument.parse(utf8.decode(response.bodyBytes));
 
-      final directories = document.findAllElements('Directory');
-      for (final directory in directories) {
-        if (directory.getAttribute("allowSync") == "1" && List.from(["movie", "show"]).contains(directory.getAttribute("type"))) {
-          final key = directory.getAttribute("key");
-          print("Updating library ${directory.getAttribute("title")}");
-          var urlUpdate = Uri.https(plexServerAddress, '/library/sections/$key/refresh', {'X-Plex-Token': plexToken});
-          client.get(urlUpdate).then((response) {
-            print('Response status: ${response.statusCode}');
-            print('Response body: ${response.body}');
-          });
-        }
+    final response = await ioClient.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    final document = XmlDocument.parse(utf8.decode(response.bodyBytes));
+
+    final directories = document.findAllElements('Directory');
+    for (final directory in directories) {
+      if (directory.getAttribute("allowSync") == "1" && List.from(["movie", "show"]).contains(directory.getAttribute("type"))) {
+        final key = directory.getAttribute("key");
+        print("Updating library ${directory.getAttribute("title")}");
+        var urlUpdate = Uri.https(plexServerAddress, '/library/sections/$key/refresh', {'X-Plex-Token': plexToken});
+        final response2 = await ioClient.get(urlUpdate);
+        print('Response status: ${response2.statusCode}');
+        print('Response body: ${response2.body}');
       }
-    });
+    }
   } finally {
-    client.close();
+    ioClient.close();
   }
 }
 
