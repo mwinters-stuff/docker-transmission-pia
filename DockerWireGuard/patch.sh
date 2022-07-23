ab03e166461a56f9385568176d59dee90ec0ab76
Mathew Winters <mathew@winters.org.nz>
2022-07-23 13:00:28 +1200

    update patch with kube changes

--- DockerWireGuard/patch.sh
+++ DockerWireGuard/patch.sh
@@ -3,8 +3,8 @@
 echo "patching manual-connections"
 FILENAME="manual-connections/connect_to_wireguard_with_token.sh"
 MATCH='\[Interface\]'
-ADD1='PostUp = DROUTE=\\\$(ip route | grep default | cut -d \\\" \\\" -f 3); HOMENET=10.43.0.0/16; HOMENET2=10.42.0.0/16; ip route add \\\$HOMENET2 via \\\$DROUTE; ip route add \\\$HOMENET via \\\$DROUTE; iptables -I OUTPUT -d \\\$HOMENET -j ACCEPT; iptables -A OUTPUT -d \\\$HOMENET2 -j ACCEPT; iptables -A OUTPUT ! -o pia -m mark ! --mark \\\$(wg show pia fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'
-ADD2='PreDown = HOMENET=10.43.0.0/16; HOMENET2=10.42.0.0/16; ip route del \\\$HOMENET3 via $DROUTE;ip route del \\\$HOMENET2 via \\\$DROUTE; ip route del \\\$HOMENET via \\\$DROUTE; iptables -D OUTPUT ! -o %i -m mark ! --mark \\\$(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT; iptables -D OUTPUT -d \\\$HOMENET -j ACCEPT; iptables -D OUTPUT -d \\\$HOMENET2 -j ACCEPT'
+ADD1='PostUp = DROUTE=\\\$(ip route | grep default | cut -d \\\" \\\" -f 3); KUBENET2=10.43.0.0/16; KUBENET1=10.42.0.0/16; ip route add \\\$KUBENET2 via \\\$DROUTE; iptables -I OUTPUT -d \\\$KUBENET1 -j ACCEPT; iptables -A OUTPUT -d \\\$KUBENET2 -j ACCEPT; iptables -A OUTPUT ! -o pia -m mark ! --mark \\\$(wg show pia fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'
+ADD2='PreDown = KUBENET2=10.43.0.0/16; KUBENET1=10.42.0.0/16; ip route del \\\$KUBENET2 via \\\$DROUTE; iptables -D OUTPUT ! -o %i -m mark ! --mark \\\$(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT; iptables -D OUTPUT -d \\\$KUBENET1 -j ACCEPT; iptables -D OUTPUT -d \\\$KUBENET2 -j ACCEPT'
 
 sed -i "/^${MATCH}/a ${ADD1}" ${FILENAME}
 sed -i "/^${MATCH}/a ${ADD2}" ${FILENAME}
